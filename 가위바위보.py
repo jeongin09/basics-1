72b6c2684167c8a412f01585663f13f2ce727066
jeongin09 <jeonginseo47@gmail.com>
2025-06-02 20:10:31 +0900

    Add files via upload

---
+++ 가위바위보.py
@@ -0,0 +1,9 @@
+me = str(input("가위바위보 중 하나를 입력하세요: "))
+if me == '가위':
+     print('졌습니다.')
+elif me == '보':
+     print('비겼습니다.')
+elif me == '바위':
+     print('이겼습니다.')
+else:
+     print('올바르지 않습니다.')
