f2b5b9cb0cca952388f3fc76d7253842105ff0d0
jeongin09 <jeonginseo47@gmail.com>
2025-06-03 02:30:30 +0900

    Add files via upload

---
+++ 숫자 맞추기.py
@@ -0,0 +1,8 @@
+number = 6
+while True:
+     answer = int(input('숫자를 맞춰보세요 (1~10): '))
+     if answer == 6:
+         print('정답입니다!')
+         break
+     else:
+         print('틀렸습니다, 다시 입력해주세요.')
